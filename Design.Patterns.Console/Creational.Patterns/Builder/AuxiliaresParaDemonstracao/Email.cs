935fafd4ba976a69c78f8839450898a4bd258f71
tatianacardosolima <tatidornel@gmail.com>
2024-06-24 22:19:49 -0300

    Adicionado o padrão builder

---
+++ Design.Patterns.Console/Creational.Patterns/Builder/AuxiliaresParaDemonstracao/Email.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design.Patterns.Console.Creational.Patterns.Builder.AuxiliaresParaDemonstracao
+{
+    public class Email
+    {
+        public string Remetente { get; set; }
+        public List<string> Destinatarios { get; set; }
+        public string Assunto { get; set; }
+        public string Corpo { get; set; }
+        public List<Anexo> Anexos { get; set; }
+
+        public Email()
+        {
+            Destinatarios = new List<string>();
+            Anexos = new List<Anexo>();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Remetente: {Remetente}");
+            sb.AppendLine($"Destinatários: {string.Join(", ", Destinatarios)}");
+            sb.AppendLine($"Assunto: {Assunto}");
+            sb.AppendLine($"Corpo:\n{Corpo}");
+            sb.AppendLine($"Anexos: {Anexos.Count}");
+            return sb.ToString();
+        }
+    }
+
+    public class Anexo
+    {
+        public string Nome { get; set; }
+        public byte[] Conteudo { get; set; }
+    }
+}
---
+++ Design.Patterns.Console/Creational.Patterns/Builder/EmailBuilder.cs
@@ -0,0 +1,51 @@
+﻿using Design.Patterns.Console.Creational.Patterns.Builder.AuxiliaresParaDemonstracao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design.Patterns.Console.Creational.Patterns.Builder
+{
+    
+    internal class EmailBuilder
+    {
+        protected Email _email;
+        protected StringBuilder _corpo;
+
+        public EmailBuilder()
+        {
+            _email = new Email();
+            _corpo = new StringBuilder();
+        }
+        public void BuildRemetente(string remetente)
+        {
+            _email.Remetente = remetente;
+        }
+
+        public void BuildDestinatarios(List<string> destinatarios)
+        {
+            _email.Destinatarios.AddRange(destinatarios);
+        }
+        public void BuildDestinatario(string destinatario)
+        {
+            _email.Destinatarios.Add(destinatario);
+        }
+
+        public void BuildAssunto(string assunto)
+        {
+            _email.Assunto = assunto;
+        }
+
+        public void BuildAddLinhaAoConteudo(string corpo)
+        {
+            _corpo.AppendLine(corpo);
+            _email.Corpo = _corpo.ToString();
+        }
+
+        public void AddAnexo(Anexo anexo)
+        {
+            _email.Anexos.Add(anexo);
+        }
+    }
+}
